bceb228308eda804acd62f5cfa88ca2ab5af633a
Arne Seime <arne.seime@gmail.com>
2019-06-26 14:31:07 +0200

    Added missing licenses

--- schema2proto-lib/src/main/java/no/entur/schema2proto/Schema2Proto.java
+++ schema2proto-lib/src/main/java/no/entur/schema2proto/Schema2Proto.java
@@ -1,6 +1,29 @@
 
 package no.entur.schema2proto;
 
+/*-
+ * #%L
+ * schema2proto-lib
+ * %%
+ * Copyright (C) 2019 Entur
+ * %%
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * #L%
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
--- schema2proto-wire/src/main/java/com/squareup/wire/schema/ProtoMember.java
+++ schema2proto-wire/src/main/java/com/squareup/wire/schema/ProtoMember.java
@@ -15,6 +15,29 @@
  */
 package com.squareup.wire.schema;
 
+/*-
+ * #%L
+ * schema2proto-wire
+ * %%
+ * Copyright (C) 2019 Entur
+ * %%
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * #L%
+ */
+
 /**
  * Identifies a field, enum or RPC on a declaring type. Members are encoded as strings containing a type name, a hash, and a member name, like
  * {@code squareup.dinosaurs.Dinosaur#length_meters}.
